568b79340d9b743fd9cd28dfcfab15c2c51d5058
STEVO7560 <82797443+STEVO7560@users.noreply.github.com>
2021-09-26 17:56:17 -0600

    Update file_dir.sh

--- file_dir.sh
+++ file_dir.sh
@@ -1,3 +1,10 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo mkdir projects
+sudo mkdir ../projects
+sudo mkdir projects/python
+sudo mkdir projects/java
+sudo mkdir projects/javascript
+sudo mkdir projects/C++
+sudo apt-get install python3
+sudo apt-get install python
+sudo apt-get install python-pip
