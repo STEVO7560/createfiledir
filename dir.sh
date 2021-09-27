dba6efe2637d1b902edba2ab1f378152604f223d
STEVO7560 <stevobbstavig@gmail.com>
2021-09-26 21:28:17 -0600

    Changes to be committed: 	new file:   dir.sh

---
+++ dir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "                                  this is a questionair about your goals and attributes and what you would"
+echo "                                  like to do in your life"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "What would you like to do in life?"
+\
+read TODO
+echo "$TODO" >> try.txt
