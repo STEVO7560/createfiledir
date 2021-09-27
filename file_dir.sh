ef6620d6197ef619332b2d29f8088b82c7e5db04
STEVO7560 <stevobbstavig@gmail.com>
2021-09-26 18:59:45 -0600

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   file_dir.sh

--- file_dir.sh
+++ file_dir.sh
@@ -1,12 +1,5 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo mkdir ../projects
-sudo mkdir projects/python
-sudo mkdir projects/java
-sudo mkdir projects/javascript
-sudo mkdir projects/C++
-sudo apt-get install python3
-sudo apt-get install python
-sudo apt-get install python-pip
-cd ../projects/python
+#!/bin/bash
+echo "What would you like to do?"
+read TODO
+echo "so your telling me you would like to: $TODO?"
 echo "This is an example" >> try.txt
