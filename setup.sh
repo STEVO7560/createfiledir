9c7e8b3e4287585ff8aa08d7f51d30732b59fbeb
STEVO7560 <stevobbstavig@gmail.com>
2021-09-27 20:24:59 -0600

    Changes to be committed: 	new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+echo " What kind of things do you find wierd that she does?"
+echo " "
+echo ""
+echo ""
+echo ""
+echo "What would you like to do in life?"
+read TODO
+echo "$TODO" >> try.txt
+
+
